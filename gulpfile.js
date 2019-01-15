096df875e2fc4df6a938b168d4dd8c834c010f25
yanan.g055 <yanan.g055@shulidata.com>
2019-01-15 15:23:49 +0800

    正则表达式

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,59 @@
 var gulp = require('gulp');
-var browser = require('browser-sync');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer'); //自动添加css前缀
+var connect = require('gulp-connect');
+var proxy = require('http-proxy-middleware');
 
-gulp.task('default', function () {
-    browser.init({
-        //指定服务器启动根目录
-        server: "../vuebase"
-    });
-    var watcher = gulp.watch('./base/**.html');
-    watcher.on('change', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+// less 转化成 css
+gulp.task('less', function () {
+    gulp.src('./assets/less/*.less')
+    .pipe(less())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./report/assets/styles'))
+})
+
+//定义html任务
+gulp.task('html', function () {
+    gulp.src(['./*.html',
+        './base/*.html',
+        './javascript/**/*.html',
+        './javascript/**/*.js',
+        './javascript/*.html',
+        './javascript/*.js'])
+    .pipe(connect.reload());
+});
+
+// 监听文件改变
+gulp.task('watch', function () {
+    gulp.watch('./less/*.less', ['less']);
+    gulp.watch(['./*.html',
+        './base/*.html',
+        './javascript/**/*.html',
+        './javascript/**/*.js',
+        './javascript/*.html',
+        './javascript/*.js'], ['html']);
+})
+
+// 起服务
+gulp.task('connect', function () {
+    connect.server({
+        root: '',
+        livereload: true,
+        host: '10.1.8.220',
+        port: 9000, //服务器端口
+        middleware: function (connect, opt) {
+            return [
+                proxy('/datahub', {
+                    target: 'http://test.openapi.shuli.com',
+                    changeOrigin:true
+                })
+            ]
+        }
     });
 });
+
+gulp.task('live', ['watch', 'less', 'connect'])
+
---
+++ javascript/regexp/index.js
@@ -0,0 +1,12 @@
+// 正则表达式
+var app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            msg: '正则表达式'
+        }
+    },
+    methods: {
+
+    }
+})
